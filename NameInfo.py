eed5e33edc73ed294143d1a2ecd4afcab8986bd5
Philip de Bruin <phlpdbrn@gmail.com>
2015-08-31 19:25:36 +0200

    Add NameInfo

---
+++ NameInfo.py
@@ -0,0 +1,19 @@
+import re
+# When you input your name, you get some information about it
+
+name = input("What is your name? ")
+
+def count_letters(word):
+    return str(len(word) - word.count(' '))
+
+def count_capital_letters(word):
+	return str(sum(1 for c in word if c.isupper()))
+
+def count_characters(word):
+	return str(len(set(word) - set(' ')))
+
+# Now print all information
+print("Total length: " + count_letters(name))
+print("Total capital characters: " + count_capital_letters(name))
+print("Total different characters: " + count_characters(name))
+print("Inversed: " + name[::-1])
