c2df6fbe74fdab5e9274d639437d04d8cf44850c
PhiliPdB <phlpdbrn@gmail.com>
2015-10-14 17:19:55 +0200

    Solved problem 4

---
+++ Project Euler/Problem4.py
@@ -0,0 +1,22 @@
+
+# NOTES:
+# All three digit numbers are in range(100, 1000)
+
+# Checks if the number is a palindrome
+def is_palindrome(number):
+	return str(number) == str(number)[::-1]
+
+def find_palindromes(number_of_digits):
+	first = 10 ** (number_of_digits - 1)
+	second = 10 ** number_of_digits
+	palindromes = []
+	
+	for i in range(first, second):
+		for j in range(first, second):
+			if is_palindrome(i * j):
+				palindromes.append(i * j)
+
+	return palindromes
+
+
+print(max(find_palindromes(3)))
