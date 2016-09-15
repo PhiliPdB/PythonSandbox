080a8ea413ce88fc7d8ae2786c4d33f1050224b0
PhiliPdB <phlpdbrn@gmail.com>
2016-09-15 09:40:04 +0200

    Update problem 31

--- Project Euler/Problem031.py
+++ Project Euler/Problem031.py
@@ -1,6 +1,6 @@
 target = 200
 coins = [1, 2, 5, 10, 20, 50, 100, 200]
-ways = [0 for x in range(200 + 1)]
+ways = [0 for x in range(target + 1)]
 ways[0] = 1
 
 for i in range(len(coins)):
@@ -9,4 +9,4 @@ for i in range(len(coins)):
 		ways[j] += ways[j - coins[i]]
 		j += 1
 
-print(ways[-1])
+print(ways[target])
