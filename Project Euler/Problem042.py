2e1441d99d04325c532051b33f198c46810bb407
PhiliPdB <phlpdbrn@gmail.com>
2016-09-20 13:47:55 +0200

    Solve problem 42

---
+++ Project Euler/Problem042.py
@@ -0,0 +1,21 @@
+import string
+
+words = open('Problem042Data.txt').read().replace('"', '').split(',')
+triangle_words = 0
+
+def t(n):
+	return int(.5 * n * (n + 1))
+
+numbers = [t(i) for i in range(1, 1000)]
+
+for word in words:
+	word = word.lower()
+	
+	total = 0
+	for letter in word:
+		total += ord(letter) - 96
+
+	if total in numbers:
+		triangle_words += 1
+
+print(triangle_words)
