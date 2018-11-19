689270c6aae2a90e33ad2b8aeab9323b341db502
PhiliPdB <phlpdbrn@gmail.com>
2018-11-19 19:03:26 +0100

    Add timing

--- Project Euler/Problem057.py
+++ Project Euler/Problem057.py
@@ -1,4 +1,7 @@
 from fractions import Fraction
+import time
+
+start = time.time()
 
 limit = 1000
 result = 0
@@ -10,11 +13,17 @@ expansions[0] = 1
 
 for i in range(1, limit + 1):
 	# Calculate next expension
-	expansions[i] = 1 + Fraction(1, 2 + (expansions[i-1] - 1))
+	expansions[i] = 1 + Fraction(1, 1 + expansions[i-1])
 
 	# When numerator has more numbers than denominator add 1 to the result
 	if len(str(expansions[i].numerator)) > len(str(expansions[i].denominator)):
 		result = result + 1
 
 
-print(result)
+stop = time.time()
+
+# Print result
+print("Result: " + str(result))
+# Print timing
+print("Time taken: " + str((stop - start) * 1000) + "ms")
+
