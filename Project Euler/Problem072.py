db8b90c1f9ac4326be4c8a4047f135dbffa91bd0
PhiliPdB <phlpdbrn@gmail.com>
2018-11-19 17:57:41 +0100

    Add comment

--- Project Euler/Problem072.py
+++ Project Euler/Problem072.py
@@ -4,7 +4,7 @@ limit = int(1e6)
 phi = list(range(0, limit + 1))
 
 for i in range(2, limit + 1):
-	if phi[i] == i:
+	if phi[i] == i: # i is prime so reduce the multiples of this number
 		for j in range(i, limit + 1, i):
 			phi[j] = phi[j] / i * (i - 1)
 
